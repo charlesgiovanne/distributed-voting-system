9e6bb0b92f7f9feb80a2ad2536c89b1efbcebd58
Charles Sorongon <163762221+charlesgiovanne@users.noreply.github.com>
2026-05-10 23:00:21 +0800

    Update main.py

--- worker_service/main.py
+++ worker_service/main.py
@@ -10,7 +10,7 @@ def health_check():
 
 def run_worker():
     REDIS_URL = "rediss://default:gQAAAAAAAdOdAAIgcDFmOGJmYmYyMWY4Mjg0ZWQ3OGRmNDM5ZmQyN2Q2ZDRiZg@fun-gecko-119709.upstash.io:6379"
-    MONGO_URL = ""mongodb+srv://suprcharlsge:yG02vFqiiXNNoLIy@cluster0.smczchr.mongodb.net/?appName=Cluster0""
+    MONGO_URL = "mongodb+srv://suprcharlsge:yG02vFqiiXNNoLIy@cluster0.smczchr.mongodb.net/?appName=Cluster0"
     
     r = redis.from_url(REDIS_URL)
     mongo_client = MongoClient(MONGO_URL)
